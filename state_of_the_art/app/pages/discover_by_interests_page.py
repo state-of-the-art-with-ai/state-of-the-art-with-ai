7525cee32ddd693299a1882dfe9594180ef84a64
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-11 05:07:53 +0200

    manage by interest

--- state_of_the_art/app/pages/discover_by_interests_page.py
+++ state_of_the_art/app/pages/discover_by_interests_page.py
@@ -4,6 +4,8 @@ from state_of_the_art.app.pages.papers_page_utils import (
     load_papers_from_last_report,
 )
 from state_of_the_art.app.pages.render_papers import render_papers
+from state_of_the_art.paper.papers_data_loader import PapersLoader
+from state_of_the_art.search.bm25_search import Bm25Search
 from state_of_the_art.tables.interest_table import Interests
 import streamlit as st
 
@@ -15,80 +17,54 @@ st.title("Discover Papers by Interest")
 papers = None
 send_by_email = False
 
-with st.expander("Search options", expanded=True):
-    topics = Interests()
-    topics_df = topics.read()
-    topics_names = [""] + topics_df["name"].tolist()
+topics = Interests()
+topics_df = topics.read()
+topics_names =  topics_df["name"].tolist()
 
-    default_interest = (
-        0
-        if "interest" not in st.query_params
-        else topics_names.index(st.query_params["interest"])
-    )
+default_interest = (
+    0 if "interest" not in st.query_params
+    else topics_names.index(st.query_params["interest"])
+)
 
-    selected_interest = st.selectbox(
-        "Existing Interest", topics_names, index=default_interest
-    )
+selected_interest = st.selectbox(
+    "Existing Interest", topics_names, index=default_interest
+)
 
-    if selected_interest:
-        st.query_params["interest"] = selected_interest
+st.query_params["interest"] = selected_interest
+interest_name = topics_df[topics_df["name"] == selected_interest].iloc[0][
+    "name"
+]
+topic_description = topics_df[topics_df["name"] == selected_interest].iloc[0][
+    "description"
+]
 
-    interest_name = ""
-    topic_description = ""
-    if selected_interest:
-        interest_name = topics_df[topics_df["name"] == selected_interest].iloc[0][
-            "name"
-        ]
-        topic_description = topics_df[topics_df["name"] == selected_interest].iloc[0][
-            "description"
-        ]
+topic_description = st.text_area("Query / Description", value=topic_description)
+interest_name = st.text_input("Interest name", value=interest_name)
 
-    topic_description = st.text_area("Query / Description", value=topic_description)
-    interest_name = st.text_input("Interest name", value=interest_name)
+c1, c2 = st.columns(2)
+with c1:
+    if st.button("Save Interest"):
+        topics.add(name=interest_name, description=topic_description)
+        st.query_params["interest"] = interest_name
+        st.success("Interest saved successfully")
+        st.rerun()
+with c2:
+    if st.button("Delete Interest"):
+        topics.delete_by(column="name", value=interest_name)
+        st.success("Interest deleted successfully")
 
-    c1, c2 = st.columns(2)
-    with c1:
-        if st.button("Save Interest"):
-            topics.add(name=interest_name, description=topic_description)
-            st.query_params["interest"] = interest_name
-            st.success("Interest saved successfully")
-            st.rerun()
-    with c2:
-        if st.button("Delete Interest"):
-            topics.delete_by(column="name", value=interest_name)
-            st.success("Interest deleted successfully")
-
-    c1, c2 = st.columns([3, 1])
-    with c1:
-        current_profile = st.selectbox("Profile", ["jean", "gdp", "mlp", "mlops"])
-    with c2:
-        if st.button("Manage Profile"):
-            edit_profile(current_profile)
-
-    c1, c2 = st.columns([2, 3])
-
-    with c1:
-        generate_clicked = st.button("Generate new recommendations")
-
-    with c2:
-        mine_new_papers = st.toggle("Mine new papers", False)
-        send_by_email = st.toggle("Send By email", False)
-    if generate_clicked:
-        from state_of_the_art.deprecated_recommender.generator import Recommender
-
-        with st.spinner("Generating recommendations"):
-            Recommender().generate(
-                skip_register=not mine_new_papers,
-                problem_description=topic_description,
-                skip_email=not send_by_email,
-                disable_open_pdf=True,
-                disable_pdf=True,
-                number_lookback_days=lookback_days,
-            )
-
-            papers, generated_date = load_papers_from_last_report()
-
+c1, c2 = st.columns([3, 1])
+with c1:
+    current_profile = st.selectbox("Profile", ["jean", "gdp", "mlp", "mlops"])
+with c2:
+    if st.button("Manage Profile"):
+        edit_profile(current_profile)
 
 st.divider()
+
+with st.spinner("Loading papers"):
+    papers = PapersLoader().get_all_papers()
+    papers = Bm25Search(papers).search(interest_name + " " + topic_description)
+
 # render all papeers
 render_papers(papers, generated_date=generated_date)
---
+++ state_of_the_art/search/bm25_search.py
@@ -0,0 +1,43 @@
+from typing import List
+
+import nltk
+from rank_bm25 import BM25Okapi as BM25
+
+from state_of_the_art.paper.arxiv_paper import ArxivPaper
+
+
+class Bm25Search:
+    def __init__(self, papers_data: List[ArxivPaper]):
+        self.tokenizer = nltk.tokenize.RegexpTokenizer(r"\w+")
+        self.lemmatizer = nltk.stem.WordNetLemmatizer()
+        self.papers_data = papers_data
+
+        self.bm25 = self.setup_bm25()
+
+    def setup_bm25(self):
+        tokenized_corpus = [
+            self.tokenize(paper.title + " " + paper.abstract)
+            for paper in self.papers_data
+        ]
+
+        bm25 = BM25(tokenized_corpus)
+
+        return bm25
+
+    def search(self, query, n=100):
+        tokenized_query = self.tokenize(query)
+
+        matches = self.bm25.get_top_n(tokenized_query, self.papers_data, n=n)
+
+        return matches
+
+    def tokenize(self, string):
+        tokens = self.tokenizer.tokenize(string)
+        lemmas = [self.lemmatizer.lemmatize(t) for t in tokens]
+        return lemmas
+
+
+if __name__ == "__main__":
+    import fire
+
+    fire.Fire()
