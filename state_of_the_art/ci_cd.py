d97aef83178b0c9e75d4dfd43841e10b6e0b3dfe
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-22 21:50:31 +0200

    improve pull logic

--- state_of_the_art/ci_cd.py
+++ state_of_the_art/ci_cd.py
@@ -66,6 +66,8 @@ role_arn = arn:aws:iam::467863034863:user/sota_deployed_role_for_s3
 source_profile = sota_deployed_role_for_s3
         """
 
+HOME = os.environ['HOME']
+TINY_DATA_WAREHOUSE_EVENTS = f"{HOME}/.tinyws/events"
 class S3:
     def list_buckets(sel):
         os.system("aws s3api list-buckets")
@@ -74,9 +76,17 @@ class S3:
         os.system(f"aws s3 ls s3://{data_bucket}")
 
     def push_local_data(self):
-        os.system(f"aws s3 cp /Users/jean.machado/.tinyws/events s3://{data_bucket}/tinydatawerehouse_events --recursive")
+        os.system(f"aws s3 cp {TINY_DATA_WAREHOUSE_EVENTS} s3://{data_bucket}/tinydatawerehouse_events --recursive")
 
-    def pull_data(self, destination="/tmp/pulled_data"):
+    def pull_data(self):
+        destination = TINY_DATA_WAREHOUSE_EVENTS
+        if not os.path.exists(destination):
+            os.system(f"mkdir -p {destination}")
+
+        if HOME == '/Users/jean.machado':
+            print("Pulling data to", destination)
+            destination = '/tmp/tinydatawerehouse_events'
+
         os.system(f"aws s3 cp s3://{data_bucket}/tinydatawerehouse_events {destination} --recursive")
 
 class Cli:
