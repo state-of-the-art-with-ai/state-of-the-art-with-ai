ad2978b60b2c383bee6ab3d61f30b17e76a85ff8
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-24 06:20:15 +0200

    add debug shell

--- state_of_the_art/app/pages/settings_page.py
+++ state_of_the_art/app/pages/settings_page.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import subprocess 
 from state_of_the_art.ci_cd import S3
 
 st.title("Settings")
@@ -11,5 +12,16 @@ if st.button("Pull latest data"):
 if st.button("Push data"):
     with st.spinner("Pushing data"):
         S3().push_local_data()
+
+
+
+st.markdown("### Debug shell")
+
+shell_cmd = st.text_input("Shell command")
+
+if shell_cmd:
+    st.write(subprocess.check_output(shell_cmd, shell=True, text=True))
+
+
 
 
