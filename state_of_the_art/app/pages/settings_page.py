e8e3359c98a15914a3330430bba76acb0444c114
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-24 06:52:23 +0200

    add pipeline

--- state_of_the_art/app/pages/settings_page.py
+++ state_of_the_art/app/pages/settings_page.py
@@ -1,3 +1,5 @@
+import os
+from state_of_the_art.tables.user_table import UserTable
 import streamlit as st
 import subprocess 
 from state_of_the_art.ci_cd import S3
@@ -15,13 +17,48 @@ if st.button("Push data"):
 
 
 
+import streamlit as st
+
 st.markdown("### Debug shell")
 
 shell_cmd = st.text_input("Shell command")
 
 if shell_cmd:
-    st.write(subprocess.check_output(shell_cmd, shell=True, text=True))
+    p = subprocess.Popen(shell_cmd, shell=True, text=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, error  = p.communicate()
+
+    st.write(error)
+    st.write(out)
 
 
 
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if UserTable().check_password("root", st.session_state["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop() 
+
+
+st.title("After password")
---
+++ state_of_the_art/tables/user_table.py
@@ -0,0 +1,26 @@
+from state_of_the_art.tables.base_table import BaseTable
+
+
+class UserTable(BaseTable):
+    table_name = "user"
+    schema = {
+        "email": {"type": str},
+        "password_hash": {"type": str},
+    }
+
+    def add_user(self, email: str, password: str):
+        df = self.read()
+        if email in df["email"].values:
+            raise ValueError(f"User with email {email} already exists")
+        self.add(email=email, password_hash=password)
+
+    def check_password(self, email: str, given_password: str) -> bool:
+        df = self.read()
+        if email not in df["email"].values:
+            return False
+        password = df.loc[df["email"] == email, "password_hash"].values[0]
+        return password == given_password
+
+if __name__ == "__main__":
+    import fire
+    fire.Fire()
