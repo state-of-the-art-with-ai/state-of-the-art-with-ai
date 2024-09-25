597a3e0c0fa2c3c23e7cfd239729401ce544a951
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-25 12:35:58 +0200

    production mode = true

--- start_monorepo.sh
+++ start_monorepo.sh
@@ -8,4 +8,4 @@ sota container_startup setup
 echo "Starting scheduler"
 PYTHONUNBUFFERED=TRUE sota scheduler run_scheduler | tee /tmp/scheduler.log &
 
-streamlit run --server.address '0.0.0.0' --server.port '80' state_of_the_art/app/start.py
+streamlit run --server.address '0.0.0.0' --server.port '80' --runner.fastReruns True --global.developmentMode False  state_of_the_art/app/start.py
