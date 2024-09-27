4b05870eb8f257e2cdf8081dbd3b11f090a6cd68
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-27 16:10:50 +0200

    hability to create users

--- state_of_the_art/app/pages/lead_collection_page.py
+++ state_of_the_art/app/pages/lead_collection_page.py
@@ -2,11 +2,13 @@
 
 import streamlit as st
 
-
 st.markdown("""
 ## Welcome to our platform
 If you are interested in our idea please register your email here. 
 I will eventually get back to you.
 """)
 
-st.text_input("Type your email here")
+email = st.text_input("Type your email here")
+
+if st.button("Submit"):
+    st.success("Thank you for your interest. I will get back to you soon.")
--- state_of_the_art/app/pages/signup_and_login_page.py
+++ state_of_the_art/app/pages/signup_and_login_page.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from state_of_the_art.tables.user_table import UserTable
+
 st.title("Sign up")
 
 st.button("Alread have an account? Log in")
@@ -7,5 +9,6 @@ st.button("Alread have an account? Log in")
 email = st.text_input("Email")
 password = st.text_input("Password")
 
-if st.button("Create account")
+if st.button("Create account"):
+    UserTable().add_user(email, password)
     st.success("Account created successfully")
