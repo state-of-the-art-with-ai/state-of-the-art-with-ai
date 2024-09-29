2659d2ed818586b7c637c5985be0e117eadc9458
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-29 13:42:34 +0200

    add text feedback table

---
+++ state_of_the_art/tables/text_feedback_table.py
@@ -0,0 +1,10 @@
+from typing import Any
+from state_of_the_art.tables.base_table import BaseTable
+from enum import Enum
+
+class TextFeedbackTable(BaseTable):
+    table_name = "text_feedback"
+    schema = {"text": {"type": str}, "score": {"type": Any}, 'type': {'type': str}}
+
+class TextTypes(Enum):
+    default = 'default'
