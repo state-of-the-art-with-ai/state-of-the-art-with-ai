0f549487825ccdbed624d1095dc05d178482f0a7
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-29 12:37:39 +0200

    loading recos

--- state_of_the_art/app/pages/papers_recommended_page.py
+++ state_of_the_art/app/pages/papers_recommended_page.py
@@ -14,8 +14,6 @@ topic_description = None
 st.title("Recommendations")
 from state_of_the_art.paper.papers_data_loader import PapersLoader
 
-recommendations = RecommendationsHistoryTable().last().to_dict()
-structured = json.loads(recommendations["recommended_papers"].replace("'", '"'))
 
 PAPER_PER_TOPIC_TO_RENDER = 3
 
@@ -43,33 +41,36 @@ if generate_clicked:
 
         p.wait()
 
-papers = []
-for interest, interest_data in structured["interest_papers"].items():
-    for paper in PapersLoader().load_papers_from_urls(interest_data["papers"].keys())[
-        0:PAPER_PER_TOPIC_TO_RENDER
-    ]:
-        papers.append(paper)
+with st.spinner("Loading latest recommendations ..."):
+    recommendations = RecommendationsHistoryTable().last().to_dict()
+    structured = json.loads(recommendations["recommended_papers"].replace("'", '"'))
+    papers = []
+    for interest, interest_data in structured["interest_papers"].items():
+        for paper in PapersLoader().load_papers_from_urls(interest_data["papers"].keys())[
+            0:PAPER_PER_TOPIC_TO_RENDER
+        ]:
+            papers.append(paper)
 
 
-recommendation_metadata = {
-    "from_date": recommendations["from_date"],
-    "to_date": recommendations["to_date"],
-    "tdw_timestamp": str(recommendations["tdw_timestamp"]).split(".")[0],
-    "papers_analysed_total": recommendations["papers_analysed_total"],
-}
+    recommendation_metadata = {
+        "from_date": recommendations["from_date"],
+        "to_date": recommendations["to_date"],
+        "tdw_timestamp": str(recommendations["tdw_timestamp"]).split(".")[0],
+        "papers_analysed_total": recommendations["papers_analysed_total"],
+    }
 
-papers_metadata = {}
-for topic, metadata in structured["interest_papers"].items():
-    for paper in metadata["papers"]:
-        papers_metadata[paper] = (
-            {"labels": [topic]}
-            if paper not in papers_metadata
-            else {"labels": papers_metadata[paper]["labels"] + [topic]}
-        )
+    papers_metadata = {}
+    for topic, metadata in structured["interest_papers"].items():
+        for paper in metadata["papers"]:
+            papers_metadata[paper] = (
+                {"labels": [topic]}
+                if paper not in papers_metadata
+                else {"labels": papers_metadata[paper]["labels"] + [topic]}
+            )
 
-render_papers(
-    papers,
-    papers_metadata=papers_metadata,
-    generated_date=generated_date,
-    metadata=recommendation_metadata,
-)
+    render_papers(
+        papers,
+        papers_metadata=papers_metadata,
+        generated_date=generated_date,
+        metadata=recommendation_metadata,
+    )
