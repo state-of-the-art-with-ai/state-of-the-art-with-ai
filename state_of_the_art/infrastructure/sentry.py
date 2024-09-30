3ea9c271c05b9490ae5918c02e3767598336ce8d
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-09-30 08:34:05 +0200

    sentry defaults to empty

--- state_of_the_art/infrastructure/sentry.py
+++ state_of_the_art/infrastructure/sentry.py
@@ -4,7 +4,7 @@ import os
 
 def setup_sentry():
     sentry_sdk.init(
-        dsn=os.environ.get("SOTA_SENTRY_DSN"),
+        dsn=os.environ.get("SOTA_SENTRY_DSN", ''),
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for tracing.
         traces_sample_rate=1.0,
