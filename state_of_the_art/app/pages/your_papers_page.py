7cb5c1adf9d336fb42c85166f52b8362fd7ff861
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-10-04 11:04:55 +0200

    spinner for papers

--- state_of_the_art/app/pages/your_papers_page.py
+++ state_of_the_art/app/pages/your_papers_page.py
@@ -14,36 +14,39 @@ send_by_email = False
 
 st.title("Your papers")
 
-all_tags_df = TagsTable().read()
-all_tags = all_tags_df["tags"].to_list()
-all_tags = [tags.split(",") for tags in all_tags]
-import itertools
+with st.spinner("Loading page"):
+    all_tags_df = TagsTable().read()
+    all_tags = all_tags_df["tags"].to_list()
+    all_tags = [tags.split(",") for tags in all_tags]
+    import itertools
 
-merged = list(itertools.chain(*all_tags))
-unique = list(set(merged))
+    merged = list(itertools.chain(*all_tags))
+    unique = list(set(merged))
 
-selected_tags = st.selectbox("Filter By tags", options=['Select' ] + unique, index=0)
+    selected_tags = st.selectbox("Filter By tags", options=['Select' ] + unique, index=0)
 
-if selected_tags == 'Select':
-    selected_tags = unique
-else:
-    selected_tags = [selected_tags]
+    if selected_tags == 'Select':
+        selected_tags = unique
+    else:
+        selected_tags = [selected_tags]
 
-all_papers_selected = all_tags_df[
-    all_tags_df["tags"].str.contains("|".join(selected_tags))
-]
-all_papers_selected = all_papers_selected["paper_id"].to_list()
+with st.spinner("Loading Papers"):
 
-unique_papers = list(set(all_papers_selected))
+    all_papers_selected = all_tags_df[
+        all_tags_df["tags"].str.contains("|".join(selected_tags))
+    ]
+    all_papers_selected = all_papers_selected["paper_id"].to_list()
 
-papers = PapersLoader().load_papers_from_urls(unique_papers)
-# sort papers by the bookmarked date
-papers = sorted(
-    papers,
-    key=lambda x: all_tags_df[all_tags_df["paper_id"] == x.abstract_url][
-        "tdw_timestamp"
-    ].values[0],
-    reverse=True,
-)
+    unique_papers = list(set(all_papers_selected))
 
-PapersRenderer(disable_save_button=True, enable_tags=True).render_papers(papers)
+    papers = PapersLoader().load_papers_from_urls(unique_papers)
+    # sort papers by the bookmarked date
+    papers = sorted(
+        papers,
+        key=lambda x: all_tags_df[all_tags_df["paper_id"] == x.abstract_url][
+            "tdw_timestamp"
+        ].values[0],
+        reverse=True,
+    )
+
+    PapersRenderer(disable_save_button=True, enable_tags=True).render_papers(papers)
