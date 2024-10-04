eb43cbdd14ef5a8bab70a050ee1d7c5f9dfcd605
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-10-04 12:47:58 +0200

    fix error message

--- state_of_the_art/infrastructure/sentry.py
+++ state_of_the_art/infrastructure/sentry.py
@@ -1,8 +1,12 @@
 
 import sentry_sdk
 import os
+from state_of_the_art.config import config
 
 def setup_sentry():
+    if not config.is_production():
+        print("Skip sentry setup as not in production")
+
     sentry_sdk.init(
         dsn=os.environ.get("SOTA_SENTRY_DSN", ''),
         # Set traces_sample_rate to 1.0 to capture 100%
