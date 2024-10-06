06bebffe7c7860e5fa7faf9ee50a7bea3d0e4654
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-10-06 13:08:12 +0200

    add tags tests

--- test/test_tags.py
+++ test/test_tags.py
@@ -4,7 +4,7 @@ from state_of_the_art.tables.tags_table import TagsTable
 
 
 def test_tags():
-    user_id = '16db56fe-18d0-41fe-b56c-4b703e79a170'
+    user_id = 'a_user_id'
     tags_table = TagsTable(auth_callable=lambda: user_id)
     tags_table.add_tag_to_paper("http://arxiv.org/abs/2410.02749", "tag1")
     df = tags_table.read()
