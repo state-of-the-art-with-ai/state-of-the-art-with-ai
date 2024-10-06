f7534ee9f7ced1565fc59ac0b769c153fe3485f0
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-10-06 20:06:25 +0200

    enviroment change works indpendent the enviroment

--- state_of_the_art/tables/tables.py
+++ state_of_the_art/tables/tables.py
@@ -1,12 +1,12 @@
 import os
+from state_of_the_art.tables.tags_table import TagsTable
+from state_of_the_art.tables.user_table import UserTable
 
 class Table:
     def __init__(self):
         # set environment variable to skip auth filter
         os.environ["SKIP_AUTH_FILTER"] = "True"
 
-        from state_of_the_art.tables.tags_table import TagsTable
-        from state_of_the_art.tables.user_table import UserTable
         self.user = UserTable()
         self.tag = TagsTable()
         
